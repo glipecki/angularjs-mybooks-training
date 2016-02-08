90c785fe981dccd2cb0841eed97db53549828a91
JakubWizla <jakub.wizla@gmail.com>
2016-02-08 21:45:46 +0100

    dodawanie ksiazek istniejacego autora

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,17 +1,51 @@
 var myApp = angular.module('booksApp', ['ui.router', 'ngResource']);
 
-myApp.factory("BookResource", function($resource) {
+myApp.factory("BookResource", function ($resource) {
   return $resource("/api/books/:id");
+});
+
+myApp.factory("AuthorResource", function ($resource) {
+  return $resource("/api/author/:id");
+});
+
+myApp.factory("CategoryResource", function ($resource) {
+  return $resource("/api/category/:id");
+});
+
+myApp.factory("SeriesResource", function ($resource) {
+  return $resource("/api/series/:id");
 });
 
 myApp.service('BookService', ['BookResource', function(res) {
    this.getBooks = function() {
-      /*return [{id: 1, title: "baaaa", author: "bJohn" },
-        {id: 2, title: "bbbbb", author: "bRambo" },
-        {id: 3, title: "bcccc", author: "bBleh" } ];*/
+       return res.query();
+   };
+    this.addBook = function(book) {
+        res.save(book);
+    };
+    this.getBook = function(id) {
+        res.get(id);
+    };
+}]);
+
+myApp.service('AuthorsService', ['AuthorResource', function(res) {
+   this.getAuthors = function() {
        return res.query();
    };
 }]);
+
+
+myApp.controller('BookAddController', function($scope, authors, BookService, AuthorsService) {
+        this.authors = authors;
+        $scope.addBook = function() {
+            if (this.vm.isAddNewAuthor) {
+                this.vm.book.author.id=null;
+                this.vm.book.author.name=this.vm.typedAuthor;
+                // TODO AuthorsService.addAuthor()
+            }
+            BookService.addBook(this.vm.book);
+        }
+});
 
 myApp.config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise("/books");
@@ -33,6 +67,15 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
       controller: function($stateParams) {
         console.log($stateParams.id);
         }
+    })
+    .state('book-add', {
+      url: "/bookedit",
+      templateUrl: "views/book.edit.tpl.html",
+      controller: 'BookAddController',
+      controllerAs: 'vm',
+      resolve: {
+        authors: ['AuthorsService', function(authorService) { return authorService.getAuthors(); }]
+      }
     })
     .state('book-details', {
       url: '/books/{id:[0-9]+}',
