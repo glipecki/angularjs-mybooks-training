a0e62ee29c3a03616bd1fb15a9a2561a12a13e6c
jdudkowska <jdudkowska@consdata.pl>
2016-02-18 22:30:41 +0100

    mybooks cd

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -138,32 +138,3 @@ app.config(['growlProvider', '$httpProvider', function(growlProvider, $httpProvi
 	  growlProvider.globalTimeToLive(3000);
 }]);
 
-/**
- * Dyrektywa dla kręciołka
- */
-app.controller('CdOverlayController', [ '$rootScope', function($rootScope) {
-	var ctrl = this;
-	this.showSpinner = true;
-	$rootScope.$on('$stateChangeStart', function() {
-		ctrl.showSpinner = true;
-	});
-	
-	$rootScope.$on('$stateChangeSuccess', function() {
-		ctrl.showSpinner = false;
-	});
-	
-	$rootScope.$on('$stateChangeError', function() {
-		ctrl.showSpinner = false;
-	});
-} ]);
-
-
-app.directive('cdOverlay', function() {
-	return {
-		restrict: 'AE',
-		relace: true,
-		templateUrl: 'views/overlay.tpl.html',
-		controller: 'CdOverlayController',
-		controllerAs: 'vm'
-	}
-});
